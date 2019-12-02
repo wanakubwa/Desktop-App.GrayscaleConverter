14068dc6d81576115b928774787bb8ae1da081ef
Fabian Berda <wanakubwa@gmail.com>
2019-12-02 16:15:41 +0100

    Dzialajcy CPP z SIMD poprawna zamiana na grayscale.

--- CPPCore/GrayscaleCore.cpp
+++ CPPCore/GrayscaleCore.cpp
@@ -1,81 +1,59 @@
 #include "GrayscaleCore.h"
 #include <iostream>
-#include <dvec.h>
 
 namespace CppGrayscaleCore
 {
-	// Converting one 128-bit register to grayscale and replace in source as destination.
+	// Converting one 128-bit register to grayscale and replace in source as destination. BGRA format
 	void GrayscaleConverterCpp::MakeGrayScaleAtOneRegisterCpp(unsigned char * src, int size)
 	{
 		// Start index of src tab.
 		int srcIndex = 0;
 
-		// vecSrc - source vector (RGBA...), vecR - Red channels (R,0,0,0,R...), vecG - Red channels (0,G,0,0,0,G...), vecB - Red channels (0,0,B,0,0,0,B...) vecA - Alpha channels
+		// vecSrc - source vector (BGRA...)
 		// Load data from src into 128-bit register starting from adress at pointer src. (From 0 index so load all 16 elements x 8bit).
 		__m128i vecSrc = _mm_loadu_si128((__m128i*) &src[srcIndex]);
 
-		__m128i maskR = _mm_setr_epi16(1, 0, 0, 0, 1, 0, 0, 0);
-		__m128i maskG = _mm_setr_epi16(0, 1, 0, 0, 0, 1, 0, 0);
-		__m128i maskB = _mm_setr_epi16(0, 0, 1, 0, 0, 0, 1, 0);
-		__m128i maskA = _mm_setr_epi16(0, 0, 0, 1, 0, 0, 0, 1);
+		// Define r,g,b factors used to calculate the average.
+		const __m128i r_coef = _mm_set1_epi16((short)(0.2989*32768.0 + 0.5));  //8 coefficients - R scale factor.
+		const __m128i g_coef = _mm_set1_epi16((short)(0.5870*32768.0 + 0.5));  //8 coefficients - G scale factor.
+		const __m128i b_coef = _mm_set1_epi16((short)(0.1140*32768.0 + 0.5));  //8 coefficients - B scale factor.
 
-		// Creating factors.
-		//const __m128i factorR = _mm_set1_epi16((short)(0.29891 * 0x10000));  //8 coefficients - R scale factor.
-		//const __m128i factorG = _mm_set1_epi16((short)(0.58661 * 0x10000));  //8 coefficients - G scale factor.
-		//const __m128i factorB = _mm_set1_epi16((short)(0.11448 * 0x10000));  //8 coefficients - B scale factor.
+		// Shuffle to configuration A0A1A2A3_R0R1R2R3_G0G1G2G3_B0B1B2B3
+		// Not revers so mask is read from left (Lo) to right (Hi). And counting from righ in srcVect (Lo).
+		__m128i shuffleMask = _mm_set_epi8(15, 11, 7, 3, 14, 10, 6, 2, 13, 9, 5, 1, 12, 8, 4, 0);
+		__m128i AAAA_R0RRR_G0GGG_B0BBB = _mm_shuffle_epi8(vecSrc, shuffleMask);
 
-		const __m128 factorR = _mm_set1_ps(0.29891);  //8 coefficients - R scale factor.
-		const __m128 factorG = _mm_set1_ps(0.58661);  //8 coefficients - G scale factor.
-		const __m128 factorB = _mm_set1_ps(0.11448);  //8 coefficients - B scale factor.
-		
-		__m128i zero = _mm_setzero_si128();
+		// Put BBBB in lower part.
+		__m128i B0_XXX = _mm_slli_si128(AAAA_R0RRR_G0GGG_B0BBB, 12);
+		__m128i XXX_B0 = _mm_srli_si128(B0_XXX, 12);
 
-		// Shifting higher part of src register to lower.
-		__m128i vectSrcLowInHighPart = _mm_cvtepu8_epi16(vecSrc);
-		__m128i vectSrcHighInHighPart = _mm_unpackhi_epi8(vecSrc, zero); 
+		// Put GGGG in Lower part.
+		__m128i G0_B_XX = _mm_slli_si128(AAAA_R0RRR_G0GGG_B0BBB, 8);
+		__m128i XXX_G0 = _mm_srli_si128(G0_B_XX, 12);
 
-		//__m128i vectHighToLowShift = _mm_srli_si128(vecSrc, 8);
-		//__m128i vectLowInLowShift = vecSrc;
+		// Put RRRR in Lower part.
+		__m128i R0_G_XX = _mm_slli_si128(AAAA_R0RRR_G0GGG_B0BBB, 4);
+		__m128i XXX_R0 = _mm_srli_si128(R0_G_XX, 12);
 
-		// Multiply high parts of 16 x uint8 vectors by channels masks and save lower half. Getting each channels separatly (in two parts H and L)
-		__m128i vecR_L = _mm_mullo_epi16(vectSrcLowInHighPart, maskR);
-		__m128i vecG_L = _mm_mullo_epi16(vectSrcLowInHighPart, maskG);
-		__m128i vecB_L = _mm_mullo_epi16(vectSrcLowInHighPart, maskB);
-		__m128i vecA_L = _mm_mullo_epi16(vectSrcLowInHighPart, maskA);
+		// Unpack uint8 elements to uint16 elements.
+		__m128i B0BBB = _mm_cvtepu8_epi16(XXX_B0);
+		__m128i G0GGG = _mm_cvtepu8_epi16(XXX_G0);
+		__m128i R0RRR = _mm_cvtepu8_epi16(XXX_R0);
 
-		// Multiply lower parts of 16 x uint8 vectors by channels masks and save lower half.
-		__m128i vecR_H = _mm_mullo_epi16(vectSrcHighInHighPart, maskR);
-		__m128i vecG_H = _mm_mullo_epi16(vectSrcHighInHighPart, maskG);
-		__m128i vecB_H = _mm_mullo_epi16(vectSrcHighInHighPart, maskB);
-		__m128i vecA_H = _mm_mullo_epi16(vectSrcHighInHighPart, maskA);
+		// Multiply each channel by separate factor.
+		__m128i B0BBB_mul = _mm_mulhrs_epi16(B0BBB, b_coef);
+		__m128i G0GGG_mul = _mm_mulhrs_epi16(G0GGG, g_coef);
+		__m128i R0RRR_mul = _mm_mulhrs_epi16(R0RRR, r_coef);
 
-		//// Multiply high parts of 16 x uint8 vectors by channels masks and save lower half. Getting each channels separatly (in two parts H and L)
-		//__m128i vecR_L = _mm_mulhi_epi16(vectLowInLowShift, maskR);
-		//__m128i vecG_L = _mm_mulhi_epi16(vectLowInLowShift, maskG);
-		//__m128i vecB_L = _mm_mulhi_epi16(vectLowInLowShift, maskB);
-		//__m128i vecA_L = _mm_mulhi_epi16(zero, maskA);
+		// Add R+B+G = Gray.
+		__m128i BGR_gray = _mm_add_epi16(_mm_add_epi16(B0BBB_mul, G0GGG_mul), R0RRR_mul);
 
-		//// Multiply lower parts of 16 x uint8 vectors by channels masks and save lower half.
-		//__m128i vecR_H = _mm_mulhi_epi16(vectHighToLowShift, maskR);
-		//__m128i vecG_H = _mm_mulhi_epi16(vectHighToLowShift, maskG);
-		//__m128i vecB_H = _mm_mulhi_epi16(vectHighToLowShift, maskB);
-		//__m128i vecA_H = _mm_mulhi_epi16(zero, maskA);
-
-		// Lower and high masks using to packing.
-		__m128i maskLo = _mm_setr_epi8(0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 14, 12, 10, 8, 6, 4, 2, 0);
-		__m128i maskHi = _mm_setr_epi8(14, 12, 10, 8, 6, 4, 2, 0, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80);
-
-		// Packed the High and Lowe part of register into one 16 x 8bit registers of each channels.
-		__m128i R = _mm_or_si128(_mm_shuffle_epi8(vecR_L, maskLo), _mm_shuffle_epi8(vecR_H, maskHi));
-		__m128i G = _mm_or_si128(_mm_shuffle_epi8(vecG_L, maskLo), _mm_shuffle_epi8(vecG_H, maskHi));
-		__m128i B = _mm_or_si128(_mm_shuffle_epi8(vecB_L, maskLo), _mm_shuffle_epi8(vecB_H, maskHi));
-		__m128i A = _mm_or_si128(_mm_shuffle_epi8(vecA_L, maskLo), _mm_shuffle_epi8(vecA_H, maskHi));
-
-		// Added all sub vectors to get in result one 128-bit vector with all edited channels.
-		__m128i resultVect = _mm_add_epi8(_mm_add_epi8(R, G), _mm_add_epi8(B, A));
+		// Shuffle to set each channel to oryginal destination position.
+		__m128i grayMsk = _mm_setr_epi8(0, 0, 0, 0, 2, 2, 2, 2, 4, 4, 4, 4, 6, 6, 6, 6);
+		__m128i vectGray = _mm_shuffle_epi8(BGR_gray, grayMsk);
 
 		// Put result vector into array to return as src pointer.
-		_mm_store_si128((__m128i*)&src[srcIndex], resultVect);
+		_mm_store_si128((__m128i*)&src[srcIndex], vectGray);
 	}
 }
 
--- CPPCore/GrayscaleCore.h
+++ CPPCore/GrayscaleCore.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <dvec.h>
+
 namespace CppGrayscaleCore
 {
 	class GrayscaleConverterCpp
@@ -7,5 +9,7 @@ namespace CppGrayscaleCore
 		// All logical functions converting using for grayscale must by declare here.
 		// Only functions no data conversions methods.
 		void MakeGrayScaleAtOneRegisterCpp(unsigned char *src, int size);
+	private:
+		__m128i _mm_mulif_epi16(__m128i inputIntVect, __m128 InputFloatVect);
 	};
 }
